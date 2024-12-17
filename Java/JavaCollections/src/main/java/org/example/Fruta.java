45846d8b1c5eafebab760a2d63bf8e3bfe5fc643
Arlindo Junior <arlindo.junior@sga.pucminas.br>
2024-12-17 18:39:05 -0300

    Collections MAP, SET E LIST

---
+++ Java/JavaCollections/src/main/java/org/example/Fruta.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Fruta implements Produto{
+    @Override
+    public String getNome() {
+        return "Fruta";
+    }
+}
---
+++ Java/JavaCollections/src/main/java/org/example/Main.java
@@ -0,0 +1,131 @@
+package org.example;
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Usando List - ArrayList
+//        List<Produto> produtos = new ArrayList<>();
+//        Pao pao = new Pao();
+//        Fruta fruta = new Fruta();
+//        produtos.add(pao);
+//        produtos.add(fruta);
+//
+//        for (Produto produto : produtos){
+//            System.out.println(produto.getNome());
+//        }
+
+//        Usando List - LinkedList
+//        List<Produto> produtos2 = new LinkedList<>();
+//        Pao pao2 = new Pao();
+//        Fruta fruta2 = new Fruta();
+//        Pao pao3 = new Pao();
+//
+//        produtos2.add(fruta2);
+//        produtos2.add(pao2);
+//        produtos2.add(pao3);
+//
+//        for (Produto produto : produtos2) {
+//            System.out.println(produto.getNome());
+//        }
+
+//        Usando Set - HashSet
+//        Set<Produto> setProdutos = new HashSet<>();
+//        Pao pao = new Pao();
+//        Fruta fruta = new Fruta();
+//        Fruta fruta2 = new Fruta();
+//
+//
+//        setProdutos.add(fruta);
+//        setProdutos.add(fruta2);
+//        setProdutos.add(pao);
+//
+//        for (Produto produto : setProdutos){
+//            System.out.println(produto.getNome());
+//        }
+//        System.out.println();
+//        System.out.println("Testanto a exclusão de duplicidade");
+//
+//        Set<String> setNome = new HashSet<>();
+//        setNome.add("Pao");
+//        setNome.add("Fruta");
+//        setNome.add("Fruta2");
+//        setNome.add("Pao");
+//
+//        for (String s : setNome){
+//            System.out.println(s);
+//        }
+//
+//        System.out.println("Exclusão de duplicidade funcionando corretamente");
+//        System.out.println();
+//        System.out.println("Excluindo Pao");
+//        setNome.remove("Pao");
+//
+//        for (String s : setNome){
+//            System.out.println(s);
+//        }
+
+
+//        Usando Set - LinkedHashSet
+//        Set<Produto> produtos = new LinkedHashSet<>();
+//
+//        Pao pao = new Pao();
+//        Fruta fruta = new Fruta();
+//        Pao pao2 = new Pao();
+//
+//        produtos.add(pao);
+//        produtos.add(fruta);
+//        produtos.add(pao2) ;
+//
+//        System.out.println("Imprimindo por ordem de inserção");
+//
+//        for (Produto produto : produtos){
+//            System.out.println(produto.getNome());
+//        }
+
+        //        Usando Set - TreeSet
+//
+//        Set<String> setTreeSet = new TreeSet<>();
+//
+//        setTreeSet.add("A");
+//        setTreeSet.add("B");
+//        setTreeSet.add("D");
+//        setTreeSet.add("C");
+//
+//        System.out.println("Imprimindo em ordem natural");
+//
+//        for (String s : setTreeSet) {
+//            System.out.println(s);
+//        }
+
+//        Usando Map - HashMap
+//        Map<String, Integer> map = new HashMap<>();
+//        map.put("A", 1);
+//        map.put("C", 3);
+//        map.put("B", 2);
+//
+//        System.out.println(map);
+//        System.out.println(map.get("A"));
+//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
+
+//        Usando Map - LinkedHashMap
+//        Map<String, Integer> map = new LinkedHashMap<>();
+//        map.put("A", 1);
+//        map.put("C", 3);
+//        map.put("B", 2);
+//
+//        System.out.println(map);
+//        System.out.println(map.get("A"));
+//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
+
+
+    }
+
+
+}
